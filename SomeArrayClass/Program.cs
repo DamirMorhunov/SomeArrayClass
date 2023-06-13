776f26379002bef7d94fb724c79b57346bd2c73e
stb.infoserv <stb.infoserv@gmail.com>
2023-06-13 19:41:19 +0300

    added tests

--- SomeArrayClass/Program.cs
+++ SomeArrayClass/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using SomeArrayClass;
+
+MyArray arr = new();
+arr.Add(1);
+arr.Add(2);
+arr.Add(3);
+arr.Add(4);
+arr.Add(5);
+arr.Print();
+//arr.Remove();
+//arr.Clear();
+//Console.WriteLine(arr.RemoveAt(2));
+Console.WriteLine(arr.Insert(2,111));
+arr.Print();
